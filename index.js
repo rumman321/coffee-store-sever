a08787c4690b60330ef69dfa5f8cb6c85c98621d
rumman321 <cset-210652@dti.ac>
2024-12-03 17:02:30 +0600

    server side added

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = "mongodb+srv://test:test1234@cluster0.ogyvr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://rrmahfuz5:2rbsfVYV13RXDmgt@cluster0.2by66.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -23,14 +24,15 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
+        
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
         const database = client.db('coffeeDB');
         const coffeeCollection = database.collection('coffee');
-
+        const userCollection=client.db('coffeeDB').collection('users')
 
         app.get('/coffee', async (req, res) => {
             const cursor = coffeeCollection.find();
@@ -74,6 +76,38 @@ async function run() {
             res.send(result);
         })
 
+
+        //user related apis
+
+        app.get('/users',async (req,res)=>{
+            const cursor=  userCollection.find()
+            const result= await cursor.toArray()
+            res.send(result)
+        })
+        app.post('/users', async(req, res)=>{
+            const newUser=req.body
+            console.log('create new user ' ,newUser)
+            const result= await userCollection.insertOne(newUser)
+            res.send(result)
+        })
+
+        app.patch('/users', async(req,res)=>{
+            const email= req.body.email
+            const filter={email}
+            const updatedDoc={
+                $set: {
+                    lastSignInTime:req.body?.lastSignInTime
+                }
+            }
+            const result= await userCollection.updateOne(filter,updatedDoc)
+            res.send(res)
+        })
+        app.delete('/users/:id', async(req,res)=>{
+            const id=req.params.id
+            const query={_id: new ObjectId(id)}
+            const result=await userCollection.deleteOne(query)
+            res.send(result)
+        })
 
 
     } finally {
